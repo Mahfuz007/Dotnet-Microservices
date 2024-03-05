70160e1b113b716d165dab7790f463a1061e3534
Mahfuz <aamahfuz2@gmail.com>
2024-03-05 22:58:25 +0600

    Feat: added discount api

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,47 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productId)
+        {
+            var coupon = await _repository.GetDiscount(productId);
+
+            return Ok(coupon);  
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            var result = await _repository.CreateDiscount(coupon);
+            return Ok(coupon);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            var result =  await _repository.UpdateDiscount(coupon);
+            return Ok(coupon);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productId)
+        {
+            var result = await _repository.DeleteDiscount(productId);
+            return Ok(result);
+        }
+    }
+}
